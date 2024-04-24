b3dbc527e6cd0afa388bad38a39b1eb6796725f8
localuser <localuser@VEA.LV>
2024-04-24 10:30:37 +0300

    Add: MyTreeNode class

--- src/datastr/MyTreeNode.java
+++ src/datastr/MyTreeNode.java
@@ -1,5 +1,47 @@
 package datastr;
 
-public class MyTreeNode {
-
+public class MyTreeNode<Ttype> {
+	//1. variables
+	private Ttype element;
+	private MyTreeNode parent = null;
+	private MyTreeNode rightChild = null;
+	private MyTreeNode leftChild = null;
+	//2. get and set
+	public Ttype getElement() {
+		return element;
+	}
+	public void setElement(Ttype element) {
+		if(element!=null)
+			this.element = element;
+		else
+			this.element = (Ttype)new Object();
+	}
+	public MyTreeNode getParent() {
+		return parent;
+	}
+	public void setParent(MyTreeNode parent) {
+		this.parent = parent;
+	}
+	public MyTreeNode getRightChild() {
+		return rightChild;
+	}
+	public void setRightChild(MyTreeNode rightChild) {
+		this.rightChild = rightChild;
+	}
+	public MyTreeNode getLeftChild() {
+		return leftChild;
+	}
+	public void setLeftChild(MyTreeNode leftChild) {
+		this.leftChild = leftChild;
+	}
+	
+	//3. constructors
+	public MyTreeNode(Ttype element) {
+		setElement(element);
+	}
+	//4. toString
+	public String toString() {
+		return ""+element;
+	}
+	
 }
